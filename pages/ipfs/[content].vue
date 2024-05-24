2192c7b17220e185835f9baba1c186e90dff36e4
Preschian Febryantara <preschian@users.noreply.github.com>
2024-05-24 14:29:39 +0700

    add ipfs pages

---
+++ pages/ipfs/[content].vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="font-family: monospace">
+    <h1>Capture Iframe</h1>
+    <h2>{{ status }}</h2>
+
+    <div>
+      <h3>Iframe:</h3>
+      <iframe :src="iframeUrl" frameborder="0" width="600" height="600"></iframe>
+    </div>
+
+    <div v-if="image">
+      <h3>Preview:</h3>
+      <img :src="image" alt="" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const status = ref('')
+const image = ref('')
+
+const route = useRoute()
+const { content } = route.params
+const { hash } = route.query
+const iframeUrl = `https://image.w.kodadot.xyz/ipfs/${content}/?hash=${hash}`
+
+onMounted(() => {
+  window.addEventListener('message', async (event) => {
+    if (event.origin.includes('kodadot.xyz') && event.data?.type === 'kodahash/render/completed' && event.data.payload?.image) {
+      const base64Response = await fetch(event.data.payload.image)
+      const blob = await base64Response.blob()
+
+      status.value = 'done'
+      image.value = URL.createObjectURL(blob)
+    }
+  })
+})
+</script>
