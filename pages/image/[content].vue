cbe35f10cae46880c3fcabc4241578bc16e38413
Preschian Febryantara <preschian@users.noreply.github.com>
2024-05-24 15:30:46 +0700

    * feat(image): add new dynamic image component with iframe fallback

---
+++ pages/image/[content].vue
@@ -0,0 +1,35 @@
+<template>
+  <img v-if="image" :src="image" width="600" height="600" />
+  <div v-else>
+    <h3>Iframe:</h3>
+    <iframe :src="iframeUrl" frameborder="0" width="600" height="600"></iframe>
+  </div>
+</template>
+
+<script setup>
+const status = ref('')
+const image = ref('')
+
+const route = useRoute()
+const { content } = route.params
+const { hash } = route.query
+const iframeUrl = `https://image.w.kodadot.xyz/ipfs/${content}/?hash=${hash}`
+
+onMounted(() => {
+  window.addEventListener('message', async (event) => {
+    if (event.origin.includes('kodadot.xyz') && event.data?.type === 'kodahash/render/completed' && event.data.payload?.image) {
+      const base64Response = await fetch(event.data.payload.image)
+      const blob = await base64Response.blob()
+
+      status.value = 'done'
+      image.value = URL.createObjectURL(blob)
+    }
+  })
+})
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
